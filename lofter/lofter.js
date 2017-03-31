01d1129ddc72925338e5da45831ec2af1747f5ea
jinglin <1430622494@qq.com>
2017-03-31 19:38:25 +0800

    lofter

--- lofter/lofter.js
+++ lofter/lofter.js
@@ -1,3 +1,14 @@
 /**
  * Created by lin on 2017/3/28.
  */
+var carousels=document.querySelectorAll(".carousel");
+var carouselIndex=carousels.length-1;
+var carouselBox=document.querySelector(".carousel-container");
+function onclick() {
+	for(var i=0;i<carouselIndex;i++){
+		carousels[i].addEventListener("onclick",function () {
+			var cW=carouselBox.clientLeft+carousels[i].clientWidth;
+		});
+	}
+}
+onclick();
