e21c0df9fcd46139baed626bd340a7efe36fefb7
wumengdi <2875877196@qq.com>
2017-04-10 19:55:26 +0800

    screen4

--- lofter/lofter.js
+++ lofter/lofter.js
@@ -1,3 +1,13 @@
 /**
  * Created by lin on 2017/3/28.
  */
+
+window.onscroll=function() {
+    if(window.document.body.scrollTop<=500||window.document.body.scrollTop>=3000){
+        document.getElementById("fix").style.display = "none"
+    }
+    else{
+        document.getElementById("fix").style.display = "block"
+    }
+};
+
